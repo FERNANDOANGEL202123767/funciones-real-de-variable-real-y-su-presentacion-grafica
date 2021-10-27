a8c721171c33095414442136ad29b07bb7c7fa0a
FERNANDOANGEL202123767 <fernandoangelhernandez62@gmail.com>
2021-10-27 18:08:22 -0500

    solucionando la documentación

--- trozo.m
+++ trozo.m
@@ -1,3 +1,12 @@
+% Octave Script
+% Title			    :funcion real de variables real
+% Description		:Script para recordar funciones reales
+% Author		    :fernando angel garcia hernandez
+% Date		    	:27/10/2021
+% Version		    :1
+% Usage			    :octave
+%               :https://octaveintro.readthedocs.io/en/latest/index.html
+
 %funcion trozos
 %si f:D?I | f(x)=(-x+2 si x<-1)(x si -1<=x<=2)(2 si x>2)
 f=@(x)(-x+2).*(x<-1)+(x).*(-1<=x).*(x<=2)+(2).*(x>2);
