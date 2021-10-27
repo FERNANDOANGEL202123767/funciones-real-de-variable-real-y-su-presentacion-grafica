d1960bd1c4a4de3550e2c55fe1eb07fc7efb2740
FERNANDOANGEL202123767 <fernandoangelhernandez62@gmail.com>
2021-10-27 18:56:36 -0500

    solucionando la documentación

--- cuarto.m
+++ cuarto.m
@@ -12,10 +12,12 @@
 clear
 pkg load symbolic
 syms x
-x=[-10:1:7]
-y=x.^4+6*x.^3+9*x.^2-1
-plot(x,y)
-hold on
+x= linspace(-50,50);
+disp ('valor de la funcion');
+disp('((2*x.^2+3*x)./(x.^2+4*x+5)');
+fx=((x.^4)+(6*x.^3)+((9*x.^2)-1));
+semilogy (x,fx);
+grid on;
 ylabel('y')
 xlabel('x')
 title('dominio xE R, rango yE[-1,+oo), minimos(-3,-1)(0,-1), maximo(-3/2,65/16)')
